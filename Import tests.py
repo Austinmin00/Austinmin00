07aa804d69a7e90a08edc618acb22fe6ce4a8680
Austin Min <79177779+Austinmin00@users.noreply.github.com>
2021-09-13 15:34:15 -0400

    Delete Import tests.py

--- Import tests.py
+++
@@ -1,7 +0,0 @@
-from students import student
-
-Student1 = student("Jim", "Buisness", 3.1, False)
-Student2 = student("Carry", "Art", 2.8, True)
-
-print(Student1.name)
-print(Student.is_on_probation)
